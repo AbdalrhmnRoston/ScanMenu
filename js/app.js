62d05a596ea0cd83407443a3173f080019ad1e4f
Abdalrhman Roston <80917742+AbdalrhmnRoston@users.noreply.github.com>
2022-01-31 18:45:50 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -37,6 +37,7 @@ myrecust.onreadystatechange = function () {
         for (i in data) {
             // Create Main Section 
             let section = document.createElement('section');
+            section.setAttribute('data-section', i);
             mobile.appendChild(section);
             // Create Hedeng Section
             let headeing = document.createElement('h4');
@@ -51,6 +52,7 @@ myrecust.onreadystatechange = function () {
             for (c in data[i]) {
                 let item = document.createElement('div');
                 item.className = 'item';
+                item.id = c;
                 elItems.appendChild(item);
                 let nameItem = document.createElement('div');
                 nameItem.className = 'name';
@@ -65,7 +67,7 @@ myrecust.onreadystatechange = function () {
                 item.appendChild(sizPrice);
                 let select = document.createElement('select');
                 select.name = 'size';
-                select.id = c;
+                select.setAttribute('data-id', c);
                 sizPrice.appendChild(select);
                 let optionSmull = document.createElement('option');
                 optionSmull.value = 'smull';
@@ -100,23 +102,104 @@ myrecust.send();
 
         for (let i = 0; i < parent.length; i++) {
             if (parent.children[i].selected) {
-                el.innerHTML = obj[parent.children[i].value] + Currency;
+                el.innerHTML = obj[parent.children[i].value];
             }
         }
         parent.addEventListener('change', function (e, i) {
-            e.target.parentElement.children[1].innerHTML = obj[e.target.value] + Currency;
+            e.target.parentElement.children[1].innerHTML = obj[e.target.value];
         })
     }
 
 
+    let order = {};
+    window.localStorage.getItem('Orders') ? order = JSON.parse(window.localStorage.getItem('Orders')): '';
+
+
+    
+    let chick = setInterval(() => {
+        if (myrecust.readyState === 4 && myrecust.status === 200) {
+            let btnAdd = document.querySelectorAll('.add-item');
+
+
+            btnAdd.forEach((el) => {
+                el.addEventListener('click', function () {
+                    addInOrders(el);
+                })
+
+            })
+            return clearInterval(chick);
+        }
+    }, 10);
 
 
 
+    function addInOrders(el) {
+        let random = Math.floor(Math.random() * 10000);
+        order[random] = el.parentElement;
+
+        order[random].name = el.parentElement.children[0].innerHTML;
+        order[random].size = getSize(el.parentElement.children[2].children[0]);
+        order[random].code = el.parentElement.id;
+        order[random].price = el.parentElement.children[2].children[1].innerHTML;
+
+        window.localStorage.setItem('Orders', JSON.stringify(order));
+
+        console.log(JSON.parse(myrecust.responseText));
+        console.log(el.parentElement.parentElement.parentElement.getAttribute('data-section'));
+    }
+
+    function getSize(e) {
+        for (let i = 0; i < e.children.length; i++) {
+            if (e.children[i].selected) {
+                return e.children[i].innerHTML;
+            }
+        }
+    }
 
 
-
-
-
+    function getOrders() {
+        let totalPrice = 0;
+        let section = document.createElement('section');
+        section.className = 'invoice';
+        mobile.appendChild(section);
+        let left = document.createElement('div');
+        left.className = 'left';
+        section.appendChild(left);
+        let right = document.createElement('div');
+        right.className = 'right';
+        section.appendChild(right);
+        let h3 = document.createElement('h3');
+        h3.innerHTML = 'Arrange your order';
+        section.appendChild(h3);
+    for (i in order) {
+        totalPrice += +order[i].price;
+        let item = document.createElement('div');
+        item.className = 'item-order';
+        section.appendChild(item);
+        let name = document.createElement('div');
+        name.innerHTML = order[i].name;
+        item.appendChild(name);
+        let size = document.createElement('div');
+        size.className = 'size';
+        size.innerHTML = order[i].size;
+        item.appendChild(size);
+        let price = document.createElement('div');
+        price.className = 'price';
+        price.innerHTML = order[i].price;
+        item.appendChild(price);
+        let icon = document.createElement('i');
+        icon.classList.add('far');
+        icon.classList.add('fa-trash-alt');
+        icon.classList.add('remove');
+        item.appendChild(icon);
+        
+    }
+    let totale = document.createElement('div');
+    totale.classList.add('totale');
+    totale.innerHTML = `Totale is : ${totalPrice}:EGP`;    
+    section.appendChild(totale);
+    }
+    getOrders();
 
 
 
